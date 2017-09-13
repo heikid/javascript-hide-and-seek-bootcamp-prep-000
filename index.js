5de339cd89bed86f86d0b8bf2c12ac8c1fe012ef
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-13 19:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function getFirstSelector (selector, criteriaFn){
-  for (let i=0; i<selector.length; i++){
-    if (criteriaFn(selector[i])){
-      return selector [i]
-    }
-  }
+function getFirstSelector (selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget (){
+  return document.querySelector ('#nested .target')
+}
+
+function increaseRankBy(n){
+
 }
